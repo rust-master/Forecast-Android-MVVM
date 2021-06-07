ba0da89a2ffe3368df0de1ecb1d6567588d77838
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-07 11:14:27 +0500

    SettingsFragment.kt added

---
+++ app/src/main/java/co/zaryab/forecastandroidmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package co.zaryab.forecastandroidmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+}
